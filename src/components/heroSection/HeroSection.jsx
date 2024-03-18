bd5c8a1560c2924553d888a901b2003d5906d117
Himanshu4-Deshmukh <himanshuudeshmukh@gmail.com>
2024-03-18 20:36:51 +0530

    home

--- src/components/heroSection/HeroSection.jsx
+++ src/components/heroSection/HeroSection.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function HeroSection() {
   return (
     <div>
-        <img src="src/assets/hero.jpg" alt="" />
+        <img src="/src/assets/hero.jpg" alt="" />
     </div>
   )
 }
