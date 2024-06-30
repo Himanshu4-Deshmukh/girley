882a9265ce55b351dc0cbb47cf05deead1f7a942
Himanshu4-Deshmukh <himanshuudeshmukh@gmail.com>
2024-07-01 00:16:16 +0530

    admin update

--- src/App.jsx
+++ src/App.jsx
@@ -98,7 +98,7 @@ export const ProtectedRoute = ({children}) => {
 const ProtectedRouteForAdmin = ({children})=> {
   const admin = JSON.parse(localStorage.getItem('user'))
   
-  if(admin.user.email === 'deshmukhh517@gmail.com'){
+  if(admin.user.email === 'tulsi36meena@gmail.com'){
     return children
   }
   else{
